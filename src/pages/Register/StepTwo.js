195f12d5071eff1ee6d59490f6c261ef571bee98
npcKei <sk11xg@qq.com>
2023-04-14 21:10:25 +0800

    progess

--- src/pages/Register/StepTwo.js
+++ src/pages/Register/StepTwo.js
@@ -1,34 +1,14 @@
-import { Stack, styled, LinearProgress } from '@mui/material';
+import { Stack } from '@mui/material';
 import { memo } from 'react';
 import { TypographyInfo } from '.';
-import EmojLaught from '../../assets/icons/register/emo_laught.svg';
-
-const CustomLinearProgress = styled(LinearProgress)((theme) => ({
-	width: '497px',
-	height: '30px',
-	borderRadius: '30px',
-	background: '#F7F7F7',
-	border: '2px solid #E2E2E2',
-	'.MuiLinearProgress-bar': {
-		borderRadius: '30px',
-		'&::after': {
-			position: 'absolute',
-			content: '""',
-			right: 0,
-			top: 0,
-			background: `url(${EmojLaught})`,
-			width: '40px',
-			height: '40px',
-		},
-	},
-}));
+import SimpleProgess from '../../components/SimpleProgess';
 
 const StepTwo = () => {
 	return (
 		<Stack direction="row" justifyContent="center">
 			<TypographyInfo sx={{ fontWeight: 600, textAlign: 'center' }}>
 				Payment Transcations Sent
-				<CustomLinearProgress value={10} variant="determinate" />
+				<SimpleProgess progess={0} />
 			</TypographyInfo>
 		</Stack>
 	);
